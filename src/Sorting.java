423fa75e2cd7ab35df3e9ea724f329c4f9a2daff
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2022-12-27 12:30:28 +0100

    Update Sorting Algorythms

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,7 +1,7 @@
 public class Sorting {
     public static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            // Find the minimum element in the unsorted part of the array
+
             int minIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[minIndex]) {
@@ -9,7 +9,6 @@ public class Sorting {
                 }
             }
 
-            // Swap the minimum element with the first element of the unsorted part
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
@@ -21,13 +20,11 @@ public class Sorting {
             int current = arr[i];
             int j = i - 1;
 
-            // Shift elements of arr[0..i-1], that are greater than current, to one position ahead
             while (j >= 0 && arr[j] > current) {
                 arr[j + 1] = arr[j];
                 j--;
             }
 
-            // Insert current element at its correct position
             arr[j + 1] = current;
         }
     }
